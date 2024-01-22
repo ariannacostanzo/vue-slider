37b04e973e5ab478c1e6f66a7faaf1f00643f552
ariannacostanzo <costanzo.arianna@outlook.it>
2024-01-22 14:02:24 +0100

    added vue and initialized app

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// console.log(Vue)
+
+const {createApp} = Vue;
+
+const app = createApp ({
+    name: 'image slider',
+    data() {
+        return {
+            
+        }
+    },
+
+
+
+
+
+});
+
+
+app.mount('#root')

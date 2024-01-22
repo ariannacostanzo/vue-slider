b0adc9099bca6879ede73a7b1a712dab9eb2ea5f
ariannacostanzo <costanzo.arianna@outlook.it>
2024-01-22 14:28:19 +0100

    created set image function

--- js/script.js
+++ js/script.js
@@ -10,12 +10,30 @@ const app = createApp ({
             currentImageIndex: 0, 
         }
     },
+    methods: {
+        currentImageDisplayed(i) {
+            return this.currentImageIndex === i
+        },
+        setDisplayedImage (target) {
+            if (target === 'next') {
+                this.currentImageIndex++
+            } else if (target === 'prev') {
+                this.currentImageIndex--
+            } else {
+                this.currentImageIndex = target
+            }
+
+        }
+    },
+    computed: {
+        
+
+    }
 
 
 
 
 
 });
-
 
 app.mount('#root')
